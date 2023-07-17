06e07f8357c389955c9a3a1c9a25e25ba130b1de
rudtn4038 <mm453@naver.com>
2023-07-17 22:46:53 +0900

    contents

--- frontend/src/views/Main/Main.tsx
+++ frontend/src/views/Main/Main.tsx
@@ -1,72 +1,156 @@
 import React from 'react';
 import './Main.css'
 import cookImg from '../../assect/imgs/books.jpg';
+import { faThumbsUp as solidfaThumbsUp } from "@fortawesome/free-solid-svg-icons";
+import { faThumbsUp as regularfaThumbsUp } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function Main() {
     return (
         <div className='main'>
             <div className='main-box'>
                 <div className='contents-box'>
+                    <div className='contents-img-box'>
+                        <FontAwesomeIcon icon={regularfaThumbsUp}  className='info-btn'/>
+                        <img src={cookImg} alt=""  className='contents-img'/>
+                    </div>
                     <p className='contents-title'>title</p>
-                    <img src={cookImg} alt=""  className='contents-img'/>
-                    <p className='contents-nick'>nickname</p>
+                    <p className='contents-detail'>내용~~~~~~~~~~~~</p>
                     <div className='info-box'>
+                        <p className='contents-nick'>nickname</p>
                         <p className='date'>2023-01-07</p>
-                        <p className='info-btn'>좋아요 버튼</p>
                     </div>
                 </div>
                 <div className='contents-box'>
-                    <p>title</p>
-                    <img src="" alt="" />
-                    <p>nickname</p>
-                    <div>
-                        <p>2023-01-07</p>
-                        <p>좋아요 버튼</p>
+                    <div className='contents-img-box'>
+                        <FontAwesomeIcon icon={regularfaThumbsUp}  className='info-btn'/>
+                        <img src={cookImg} alt=""  className='contents-img'/>
+                    </div>
+                    <p className='contents-title'>title</p>
+                    <p className='contents-detail'>내용~~~~~~~~~~~~</p>
+                    <div className='info-box'>
+                        <p className='contents-nick'>nickname</p>
+                        <p className='date'>2023-01-07</p>
                     </div>
                 </div>
                 <div className='contents-box'>
-                    <p>title</p>
-                    <img src="" alt="" />
-                    <p>nickname</p>
-                    <div>
-                        <p>2023-01-07</p>
-                        <p>좋아요 버튼</p>
+                    <div className='contents-img-box'>
+                        <FontAwesomeIcon icon={regularfaThumbsUp}  className='info-btn'/>
+                        <img src={cookImg} alt=""  className='contents-img'/>
+                    </div>
+                    <p className='contents-title'>title</p>
+                    <p className='contents-detail'>내용~~~~~~~~~~~~</p>
+                    <div className='info-box'>
+                        <p className='contents-nick'>nickname</p>
+                        <p className='date'>2023-01-07</p>
                     </div>
                 </div>
                 <div className='contents-box'>
-                    <p>title</p>
-                    <img src="" alt="" />
-                    <p>nickname</p>
-                    <div>
-                        <p>2023-01-07</p>
-                        <p>좋아요 버튼</p>
+                    <div className='contents-img-box'>
+                        <FontAwesomeIcon icon={regularfaThumbsUp}  className='info-btn'/>
+                        <img src={cookImg} alt=""  className='contents-img'/>
+                    </div>
+                    <p className='contents-title'>title</p>
+                    <p className='contents-detail'>내용~~~~~~~~~~~~</p>
+                    <div className='info-box'>
+                        <p className='contents-nick'>nickname</p>
+                        <p className='date'>2023-01-07</p>
                     </div>
                 </div>
                 <div className='contents-box'>
-                    <p>title</p>
-                    <img src="" alt="" />
-                    <p>nickname</p>
-                    <div>
-                        <p>2023-01-07</p>
-                        <p>좋아요 버튼</p>
+                    <div className='contents-img-box'>
+                        <FontAwesomeIcon icon={regularfaThumbsUp}  className='info-btn'/>
+                        <img src={cookImg} alt=""  className='contents-img'/>
+                    </div>
+                    <p className='contents-title'>title</p>
+                    <p className='contents-detail'>내용~~~~~~~~~~~~</p>
+                    <div className='info-box'>
+                        <p className='contents-nick'>nickname</p>
+                        <p className='date'>2023-01-07</p>
                     </div>
                 </div>
                 <div className='contents-box'>
-                    <p>title</p>
-                    <img src="" alt="" />
-                    <p>nickname</p>
-                    <div>
-                        <p>2023-01-07</p>
-                        <p>좋아요 버튼</p>
+                    <div className='contents-img-box'>
+                        <FontAwesomeIcon icon={regularfaThumbsUp}  className='info-btn'/>
+                        <img src={cookImg} alt=""  className='contents-img'/>
+                    </div>
+                    <p className='contents-title'>title</p>
+                    <p className='contents-detail'>내용~~~~~~~~~~~~</p>
+                    <div className='info-box'>
+                        <p className='contents-nick'>nickname</p>
+                        <p className='date'>2023-01-07</p>
                     </div>
                 </div>
                 <div className='contents-box'>
-                    <p>title</p>
-                    <img src="" alt="" />
-                    <p>nickname</p>
-                    <div>
-                        <p>2023-01-07</p>
-                        <p>좋아요 버튼</p>
+                    <div className='contents-img-box'>
+                        <FontAwesomeIcon icon={regularfaThumbsUp}  className='info-btn'/>
+                        <img src={cookImg} alt=""  className='contents-img'/>
+                    </div>
+                    <p className='contents-title'>title</p>
+                    <p className='contents-detail'>내용~~~~~~~~~~~~</p>
+                    <div className='info-box'>
+                        <p className='contents-nick'>nickname</p>
+                        <p className='date'>2023-01-07</p>
+                    </div>
+                </div>
+                <div className='contents-box'>
+                    <div className='contents-img-box'>
+                        <FontAwesomeIcon icon={regularfaThumbsUp}  className='info-btn'/>
+                        <img src={cookImg} alt=""  className='contents-img'/>
+                    </div>
+                    <p className='contents-title'>title</p>
+                    <p className='contents-detail'>내용~~~~~~~~~~~~</p>
+                    <div className='info-box'>
+                        <p className='contents-nick'>nickname</p>
+                        <p className='date'>2023-01-07</p>
+                    </div>
+                </div>
+                <div className='contents-box'>
+                    <div className='contents-img-box'>
+                        <FontAwesomeIcon icon={regularfaThumbsUp}  className='info-btn'/>
+                        <img src={cookImg} alt=""  className='contents-img'/>
+                    </div>
+                    <p className='contents-title'>title</p>
+                    <p className='contents-detail'>내용~~~~~~~~~~~~</p>
+                    <div className='info-box'>
+                        <p className='contents-nick'>nickname</p>
+                        <p className='date'>2023-01-07</p>
+                    </div>
+                </div>
+                <div className='contents-box'>
+                    <div className='contents-img-box'>
+                        <FontAwesomeIcon icon={regularfaThumbsUp}  className='info-btn'/>
+                        <img src={cookImg} alt=""  className='contents-img'/>
+                    </div>
+                    <p className='contents-title'>title</p>
+                    <p className='contents-detail'>내용~~~~~~~~~~~~</p>
+                    <div className='info-box'>
+                        <p className='contents-nick'>nickname</p>
+                        <p className='date'>2023-01-07</p>
+                    </div>
+                </div>
+                <div className='contents-box'>
+                    <div className='contents-img-box'>
+                        <FontAwesomeIcon icon={regularfaThumbsUp}  className='info-btn'/>
+                        <img src={cookImg} alt=""  className='contents-img'/>
+                    </div>
+                    <p className='contents-title'>title</p>
+                    <p className='contents-detail'>내용~~~~~~~~~~~~</p>
+                    <div className='info-box'>
+                        <p className='contents-nick'>nickname</p>
+                        <p className='date'>2023-01-07</p>
+                    </div>
+                </div>
+                <div className='contents-box'>
+                    <div className='contents-img-box'>
+                        <FontAwesomeIcon icon={regularfaThumbsUp}  className='info-btn'/>
+                        <img src={cookImg} alt=""  className='contents-img'/>
+                    </div>
+                    <p className='contents-title'>title</p>
+                    <p className='contents-detail'>내용~~~~~~~~~~~~</p>
+                    <div className='info-box'>
+                        <p className='contents-nick'>nickname</p>
+                        <p className='date'>2023-01-07</p>
                     </div>
                 </div>
             </div>
